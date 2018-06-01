562c5bfc3f285460ba84768494e03c6b3592eacc
marek2222 <marekmusial2@o2.pl>
2018-06-01 17:45:02 +0200

    Pragim_Rest_04_EmployeeService: niezakończony

---
+++ Pragim_Rest_04_EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+  public class EmployeesController : ApiController
+  {
+    public IEnumerable<Employees> Get()
+    {
+      using (Entities entities = new Entities())
+      {
+        return entities.Employees.ToList();
+      }
+    }
+
+    public Employees Get(int id)
+    {
+      using (Entities entities = new Entities())
+      {
+        return entities.Employees.FirstOrDefault(e => e.ID == id);
+      }
+    }
+  }
+}
